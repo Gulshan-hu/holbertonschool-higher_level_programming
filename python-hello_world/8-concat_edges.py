ac1b7d763455e6be3c374f65a8ae58997111aeea
Gulshan <xalilovagulsen2006@gmail.com>
2026-02-08 13:37:29 +0000

    Task 6: create new sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print(str1 + chr(32) + str2 + chr(32) + str3)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str.split()[5] + " " + str.split()[6] + " with " + str.split()[0])
