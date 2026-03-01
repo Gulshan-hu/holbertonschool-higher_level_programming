96200d8c9593ab325a9251a4d9488c1ea4327cbe
Gulshan <xalilovagulsen2006@gmail.com>
2026-03-01 17:25:00 +0000

    Strict type check for integer_validator

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-"""BaseGeometry modulu üçün docstring"""
+"""
+7-base_geometry modulu
+Bu modul BaseGeometry sinfini təyin edir.
+"""
 
 
 class BaseGeometry:
     """BaseGeometry sinfi"""
 
     def area(self):
-        """area metodu implementasiya olunmayıb"""
+        """area() metodu implementasiya olunmayıb"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Dəyəri yoxlayır: integer və > 0 olmalıdır"""
+        """
+        Dəyəri integer və 0-dan böyük olub-olmamasına görə yoxlayır.
+        Args:
+            name (str): Parametrin adı (həmişə string).
+            value (int): Yoxlanılacaq dəyər.
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
