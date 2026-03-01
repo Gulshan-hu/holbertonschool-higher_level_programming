2109cc0d49edd31876bd1c3375d311a5442de3ab
Gulshan <xalilovagulsen2006@gmail.com>
2026-03-01 15:58:03 +0000

    Fix: stable integer_validator and clean tests

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""BaseGeometry module: area() and integer_validator()."""
+"""BaseGeometry module."""
 
 
 class BaseGeometry:
-    """Geometry base class."""
+    """Base class for geometry objects."""
 
     def area(self):
-        """Not implemented."""
+        """Raise exception because area is not implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Check that value is an int > 0."""
+        """Validate that value is an integer greater than 0."""
         if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
