4a41bcb20bc1e655c1a6c71c7140f0413cd8a37e
Gulshan <xalilovagulsen2006@gmail.com>
2026-03-01 15:39:44 +0000

    Fix task 4 docstring to avoid false import detection

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Defines a function that checks if an object inherited
-from a specified class (directly or indirectly).
-"""
+"""Check whether an object comes from a subclass of a given class."""
 
 
 def inherits_from(obj, a_class):
