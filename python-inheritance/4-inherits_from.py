6d19428516644303553cb020de4302028a1f7889
Gulshan <xalilovagulsen2006@gmail.com>
2026-03-01 15:38:12 +0000

    Task 4: inherits_from

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a function that checks if an object inherited
+from a specified class (directly or indirectly).
+"""
+
+
+def inherits_from(obj, a_class):
+    """Return True if obj is an instance of a subclass of a_class."""
+    return isinstance(obj, a_class) and type(obj) is not a_class
